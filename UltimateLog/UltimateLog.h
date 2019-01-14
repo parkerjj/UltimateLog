73071502a8ea3b2cb10f94b0026397b796090adb
Peigen Liu <parkerlpg@gmail.com>
2019-01-14 14:16:33 +0800

    Initial Commit

---
+++ UltimateLog/UltimateLog.h
@@ -0,0 +1,19 @@
+//
+//  UltimateLog.h
+//  UltimateLog
+//
+//  Created by Peigen.Liu on 1/14/19.
+//  Copyright © 2019 Peigen.Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UltimateLog.
+FOUNDATION_EXPORT double UltimateLogVersionNumber;
+
+//! Project version string for UltimateLog.
+FOUNDATION_EXPORT const unsigned char UltimateLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UltimateLog/PublicHeader.h>
+
+
